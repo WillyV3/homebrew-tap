5ccb6b6cf03e6cc54cb7f7ca6352a76f43412f12
William Van Sickle <vansicklewilly@gmail.com>
2025-09-26 10:46:17 -0400

    Update nextui to v0.1.5

--- Formula/nextui.rb
+++ Formula/nextui.rb
@@ -1,8 +1,8 @@
 class Nextui < Formula
   desc "TUI to scaffold Next.js projects with shadcn/ui components and authentication"
   homepage "https://github.com/WillyV3/nextjs-templater"
-  url "https://github.com/WillyV3/nextjs-templater/archive/v0.0.1.tar.gz"
-  sha256 "8636b99adf23b940cb17e307998c9ce92b6bb57e4a9904e93d4e83740c746936"
+  url "https://github.com/WillyV3/nextjs-templater/archive/v0.1.5.tar.gz"
+  sha256 "44acfc52847cf0af65c99e3776aee703007aef89343bb4c39e1ab5539bcc37bf"
   license "MIT"
 
   depends_on "go" => :build
