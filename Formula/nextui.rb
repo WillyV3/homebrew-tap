a096a1fd119d4325e44c39fc9757a30c92f79be7
William Van Sickle <vansicklewilly@gmail.com>
2025-09-26 14:57:17 -0400

    Update nextui to v0.1.38
    WillyV3 Generated with BreakingShit

--- Formula/nextui.rb
+++ Formula/nextui.rb
@@ -1,8 +1,8 @@
 class Nextui < Formula
   desc "TUI to scaffold Next.js projects with shadcn/ui components and authentication"
   homepage "https://github.com/WillyV3/nextjs-templater"
-  url "https://github.com/WillyV3/nextjs-templater/archive/v0.1.20.tar.gz"
-  sha256 "ba2b358cbb17293581572e151a385417feebc64f0fcc8cb9b7b47a9fd96c5289"
+  url "https://github.com/WillyV3/nextjs-templater/archive/v0.1.38.tar.gz"
+  sha256 "5ffff6a5251fbf23eccceee49fe88baf3119a1bd57eb8ef515b03a8133988964"
   license "MIT"
 
   depends_on "go" => :build
