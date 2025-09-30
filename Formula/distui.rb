f928f1fa5a369732d53121389aa0f1a241907444
William Van Sickle <vansicklewilly@gmail.com>
2025-09-30 00:21:20 -0400

    Fix: Update distui formula to v0.0.10 with correct SHA

--- Formula/distui.rb
+++ Formula/distui.rb
@@ -1,9 +1,10 @@
 class Distui < Formula
   desc "distui"
   homepage "https://github.com/willyv3/distui"
-  version "v0.0.1"
-  url "https://github.com/willyv3/distui/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "0a88424eb924c0bcb768bcb00c84bbe67b0f5a00ab3528f3e19c5c830f5b8c14"
+  version "v0.0.10"
+  url "https://github.com/willyv3/distui/archive/refs/tags/v0.0.10.tar.gz"
+  sha256 "677404536d87161638e0a17259df8d06da9c05fdeb6ecdbd9db8a981623148fc"
+  license "MIT"
 
   depends_on "go" => :build
 
