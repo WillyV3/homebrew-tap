da02406dfb37847b979f64f01d09c7b774c704b3
William Van Sickle <vansicklewilly@gmail.com>
2025-09-29 23:48:45 -0400

    Update distui to v0.0.1
    Generated by distui

--- Formula/distui.rb
+++ Formula/distui.rb
@@ -3,7 +3,7 @@ class Distui < Formula
   homepage "https://github.com/willyv3/distui"
   version "v0.0.1"
   url "https://github.com/willyv3/distui/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "7f2f6572011891f1551ed4fbf340d4d146ab10fb47faeccad31801b3db7242bf"
+  sha256 "0a88424eb924c0bcb768bcb00c84bbe67b0f5a00ab3528f3e19c5c830f5b8c14"
 
   depends_on "go" => :build
 
