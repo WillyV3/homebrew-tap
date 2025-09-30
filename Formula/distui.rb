9902a8d9b063897596dbe353351986abdbb6e27a
William Van Sickle <vansicklewilly@gmail.com>
2025-09-30 00:26:37 -0400

    Fix: Use pre-built binaries for distui v0.0.10

--- Formula/distui.rb
+++ Formula/distui.rb
@@ -1,15 +1,22 @@
 class Distui < Formula
-  desc "distui"
+  desc "TUI for managing Go application releases"
   homepage "https://github.com/willyv3/distui"
-  version "v0.0.10"
-  url "https://github.com/willyv3/distui/archive/refs/tags/v0.0.10.tar.gz"
-  sha256 "677404536d87161638e0a17259df8d06da9c05fdeb6ecdbd9db8a981623148fc"
+  version "0.0.10"
   license "MIT"
 
-  depends_on "go" => :build
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/WillyV3/distui/releases/download/v0.0.10/distui_0.0.10_darwin_arm64.tar.gz"
+      sha256 "85b6e4cc797f07e2938070d074e89754021217f8f54a06288a2bf6079a88b8c5"
+    else
+      url "https://github.com/WillyV3/distui/releases/download/v0.0.10/distui_0.0.10_darwin_amd64.tar.gz"
+      sha256 "7a440e37854ad5dd25e5d4460b62320926ae8b6011c2420fffa28b4ee23ed2d4"
+    end
+  end
+
 
   def install
-    system "go", "build", "-o", bin/"distui"
+    bin.install "distui"
   end
 
   test do
