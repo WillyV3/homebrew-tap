774f4bd9fda6ecd6087fcc6b7debbc3b3f8cf698
William Van Sickle <vansicklewilly@gmail.com>
2025-10-18 00:07:05 -0400

    Update todobi to v1.1.0 - Full CRUD support
    - Task creation form (press 'a' or 'n')
    - Task editing (press 'e')
    - In-TUI CRUD operations
    - No manual config editing needed

--- Formula/todobi.rb
+++ Formula/todobi.rb
@@ -1,10 +1,10 @@
 class Todobi < Formula
   desc "Beautiful terminal task manager built with Bubble Tea"
   homepage "https://github.com/WillyV3/todobi"
-  url "https://github.com/WillyV3/todobi/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "1e543a6b802e540a7731efff776359d21d168a7080929e974cef5787d9f2c009"
+  url "https://github.com/WillyV3/todobi/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "543034d55359c85c4396d40b18d17e01052a72a54c9c127ab3ac338daad17562"
   license "MIT"
-  version "1.0.0"
+  version "1.1.0"
 
   depends_on "go" => :build
 
@@ -13,6 +13,6 @@ class Todobi < Formula
   end
 
   test do
-    assert_match "todobi v1.0.0", shell_output("#{bin}/todobi version")
+    assert_match "todobi v1.1.0", shell_output("#{bin}/todobi version")
   end
 end
