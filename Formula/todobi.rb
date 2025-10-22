318fb5fe80e3084e885f47e1d8c75fb6dc49b842
William Van Sickle <vansicklewilly@gmail.com>
2025-10-22 07:28:09 -0400

    Release todobi v1.4.0

--- Formula/todobi.rb
+++ Formula/todobi.rb
@@ -1,10 +1,10 @@
 class Todobi < Formula
   desc "Beautiful terminal task manager built with Bubble Tea"
   homepage "https://github.com/WillyV3/todobi"
-  url "https://github.com/WillyV3/todobi/archive/v1.3.10.tar.gz"
-  sha256 "279f2c5e167ddb7b5d10280d73666a3544827539dc6ab4ec56b7349f3f3799c7"
+  url "https://github.com/WillyV3/todobi/archive/v1.4.0.tar.gz"
+  sha256 "7183abd514342857209331cff7cb38ad1c92fb19f5d2778bd414795560247246"
   license "MIT"
-  version "1.3.10"
+  version "1.4.0"
 
   depends_on "go" => :build
 
@@ -13,6 +13,6 @@ class Todobi < Formula
   end
 
   test do
-    assert_match "todobi v1.3.10", shell_output("#{bin}/todobi --version")
+    assert_match "todobi v1.4.0", shell_output("#{bin}/todobi --version")
   end
 end
