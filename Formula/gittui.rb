3c7972671fe0d3320c655691d6be5341fc1ca8d0
williavs <vansicklewilly@gmail.com>
2025-11-07 17:30:05 -0500

    Update gittui to v1.3.4

--- Formula/gittui.rb
+++ Formula/gittui.rb
@@ -1,8 +1,8 @@
 class Gittui < Formula
   desc "GitHub profile viewer TUI - Terminal dashboard for GitHub activity"
   homepage "https://github.com/willyv3/gittui"
-  url "https://github.com/WillyV3/gittui/archive/v1.3.3.tar.gz"
-  sha256 "9db12ca08f8190a83ebe37cb4c3edaadacb7b6cf4ace8f4edddc564a107da1ec"
+  url "https://github.com/WillyV3/gittui/archive/v1.3.4.tar.gz"
+  sha256 "f161cdd6a61c0a6fd7f25faee14dd00971642373fe747b844791b3be5e11b5f1"
   license "MIT"
 
   depends_on "go" => :build
